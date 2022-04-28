2be7160851a68c94618508e07f4dcfe254d3a5c1
Paulo Tiago <paulotiagocm@hotmail.com>
2022-04-28 13:57:06 -0300

    hello world nes.css page

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function digabomdia(){
+    let name = document.getElementById("inputname").value
+    let containerMsg = document.getElementById("hellomsg")
+    containerMsg.innerHTML = `<p>Hello world ${name}</p>`
+    containerMsg.style = ""
+}
